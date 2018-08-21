76ab3f2ff0cb5a4d629de5ddb65608dda2f12817
kaivyn.molis <kaivyn.molis@yandex.ru>
2018-08-21 17:59:48 +0300

    single continue dialog and email history

--- ConnectingApplication/Entity/Characters/Player.cs
+++ ConnectingApplication/Entity/Characters/Player.cs
@@ -12,38 +12,48 @@ namespace ConnectingApplication.Characters
 	public class Player : Character
 	{
 		private Dictionary<string, Queue<DialogueNode>> textMessages;
+		private Dictionary<string, Queue<DialogueNode>> emailMessages;
 		private List<string> phoneContacts;
 
 
-        public Player()
-        {
-            phoneContacts = new List<string>();
-            textMessages = new Dictionary<string, Queue<DialogueNode>>();
-        }
+		public Player()
+		{
+			phoneContacts = new List<string>();
+			textMessages = new Dictionary<string, Queue<DialogueNode>>();
+		}
 
 
 		public Queue<DialogueNode> GetMessageHistory(string charId)
 		{
-            if (!textMessages.ContainsKey(charId))
-                return new Queue<DialogueNode>();
-            return textMessages[charId];
+			if (!textMessages.ContainsKey(charId))
+				return new Queue<DialogueNode>();
+			return textMessages[charId];
 		}
 
-        public void AddMessage(string charId, DialogueNode dialogueNode)
-        {
-            if (!textMessages.ContainsKey(charId))
-                textMessages.Add(charId, new Queue<DialogueNode>());
-            textMessages[charId].Enqueue(dialogueNode);
-        }
+		public void AddMessage(string charId, DialogueNode dialogueNode, DialogueMode mode)
+		{
+			if (mode == DialogueMode.sms)
+			{
+				if (!textMessages.ContainsKey(charId))
+					textMessages.Add(charId, new Queue<DialogueNode>());
+				textMessages[charId].Enqueue(dialogueNode);
+			}
+			if (mode == DialogueMode.email)
+			{
+				if (!emailMessages.ContainsKey(charId))
+					emailMessages.Add(charId, new Queue<DialogueNode>());
+				emailMessages[charId].Enqueue(dialogueNode);
+			}
+		}
 
-        public IList<string> GetPhoneContacts()
-        {
-            return phoneContacts.AsReadOnly();
-        }
+		public IList<string> GetPhoneContacts()
+		{
+			return phoneContacts.AsReadOnly();
+		}
 
-        public void AddContact(string character)
-        {
-            phoneContacts.Add(character);
-        }
+		public void AddContact(string character)
+		{
+			phoneContacts.Add(character);
+		}
 	}
 }
--- ConnectingApplication/Managers/DialogManager.cs
+++ ConnectingApplication/Managers/DialogManager.cs
@@ -8,126 +8,152 @@ using System.Linq;
 
 namespace ConnectingApplication.Managers
 {
-    public class DialogManager
-    {
-        private List<Dialog> activeDialogs;
-        private Dictionary<string, Dialog> activeMessageDialogs;
-        private List<Dialog> discussions;
-        private List<Dialog> textMessages;
 
-        public int ActiveDialogsCount { get { return activeDialogs.Count; } }
-        public IList<Dialog> ActiveDialogs { get { return activeDialogs.AsReadOnly(); } }
+	public class DialogManager
+	{
+		private List<Dialog> activeDialogs;
+		private Dictionary<string, Dialog> activeMessageDialogs;
+		private List<Dialog> discussions;
+		private List<Dialog> textMessages;
 
-        [Obsolete("Don't use outside the ConnectingApp.")]
-        public DialogManager()
-        {
-            activeDialogs = new List<Dialog>();
-            activeMessageDialogs = new Dictionary<string, Dialog>();
-        }
+		public int ActiveDialogsCount { get { return activeDialogs.Count; } }
+		public IList<Dialog> ActiveDialogs { get { return activeDialogs.AsReadOnly(); } }
 
-
-        private void SetResultsForNode(DialogueNode dialogueNode)
-        {
-            dialogueNode.InvokeResult();
-            // TODO: сохранить 
-        }
+		[Obsolete("Don't use outside the ConnectingApp.")]
+		public DialogManager()
+		{
+			activeDialogs = new List<Dialog>();
+			activeMessageDialogs = new Dictionary<string, Dialog>();
+		}
 
 
-        public void AddDiscussion(Dialog dialog)
-        {
-            discussions.Add(dialog);
-        }
+		private void SetResultsForNode(DialogueNode dialogueNode)
+		{
+			dialogueNode.InvokeResult();
+			// TODO: сохранить 
+		}
 
-        public List<DialogueNode> StartDialog(string charId, DialogueMode dialogueMode)
-        {
-            Dialog newOne = ConnectingAppManager.CharacterManager.GetDialog(charId, dialogueMode);
-            newOne.currentBlock = IsDialogLonely(newOne) ? Core.Dialogues.DialogueBlock.BlockType.body : Core.Dialogues.DialogueBlock.BlockType.hi;
-            activeDialogs.Add(newOne);
-            return ContinueDialog();
-        }
 
-        /// <summary>
-        /// Чтобы запросить новые ноды 
-        /// </summary>
-        /// <param name="nodeId"></param>
-        /// <returns></returns>
-        public List<DialogueNode> ContinueDialog(DialogueNode dialogueNode = null)
-        {
-            var curDialog = activeDialogs.Last();
-            int nodeId = -1;
+		public void AddDiscussion(Dialog dialog)
+		{
+			discussions.Add(dialog);
+		}
 
-            if (dialogueNode != null)
-            {
-                SetResultsForNode(dialogueNode);
-                nodeId = dialogueNode.Id;
-            }
-            else
-            {
-                if (curDialog.currentNode != null)
-                    nodeId = curDialog.currentNode.Id;
-            }
+		public List<DialogueNode> StartDialog(string charId, DialogueMode dialogueMode)
+		{
+			Dialog newOne = ConnectingAppManager.CharacterManager.GetDialog(charId, dialogueMode);
+			newOne.currentBlock = IsDialogLonely(newOne) ? Core.Dialogues.DialogueBlock.BlockType.body : Core.Dialogues.DialogueBlock.BlockType.hi;
+			activeDialogs.Add(newOne);
+			return ContinueDialog();
+		}
 
-            var nodes = curDialog.TakeNextNodes(nodeId);
+		/// <summary>
+		/// Чтобы запросить новые ноды 
+		/// </summary>
+		/// <param name="nodeId"></param>
+		/// <returns></returns>
+		public List<DialogueNode> ContinueDialog(DialogueNode dialogueNode = null)
+		{
+			var curDialog = activeDialogs.Last();
+			int nodeId = -1;
 
-            if (nodes.Count != 0)
-            {
-                return nodes;
-            }
-            else
-            {
-                activeDialogs.Remove(curDialog);
-                if (!curDialog.Reusable)
-                {
-                    foreach (string ch in curDialog.Participants)
-                    {
-                        var npc = ConnectingAppManager.CharacterManager.GetNPC(ch);
-                        npc.AvailableDialogs[curDialog.DialogueMode].Remove(curDialog);
-                        if (npc.AvailableDialogs[curDialog.DialogueMode].Count == 0)
-                            npc.ActivateObject(false, curDialog.DialogueMode);
-                    }
-                }
-                if (activeDialogs.Count == 0)
-                    return nodes;
-                else return ContinueDialog();
-            }
-        }
+			if (dialogueNode != null)
+			{
+				SetResultsForNode(dialogueNode);
+				nodeId = dialogueNode.Id;
+			}
+			else
+			{
+				if (curDialog.currentNode != null)
+					nodeId = curDialog.currentNode.Id;
+			}
 
-        public void BreakingDialog(string character, string dialogId, DialogueMode dialogueMode, EDialogueResultType breakingType)
-        {
-            var npc = ConnectingAppManager.CharacterManager.GetNPC(character);
-            npc.AvailableDialogs[dialogueMode].Find(s => s.Id.Equals(dialogId)).ActivateResult(breakingType);
-        }
+			var nodes = curDialog.TakeNextNodes(nodeId);
 
-        public void BreakingDialog(EDialogueResultType breakingType)
-        {
-            activeDialogs.Last().ActivateResult(breakingType);
-        }
+			if (nodes.Count != 0)
+			{
+				return nodes;
+			}
+			else
+			{
+				activeDialogs.Remove(curDialog);
+				if (!curDialog.Reusable)
+				{
+					foreach (string ch in curDialog.Participants)
+					{
+						var npc = ConnectingAppManager.CharacterManager.GetNPC(ch);
+						npc.AvailableDialogs[curDialog.DialogueMode].Remove(curDialog);
+						if (npc.AvailableDialogs[curDialog.DialogueMode].Count == 0)
+							npc.ActivateObject(false, curDialog.DialogueMode);
+					}
+				}
+				if (activeDialogs.Count == 0)
+					return nodes;
+				else return ContinueDialog();
+			}
+		}
 
-        public List<DialogueNode> ContinueMessengerDialog(int nodeId, string dialogId, string charId)
-        {
-            Dialog curDialog = activeMessageDialogs[dialogId];
-            curDialog.currentNode = curDialog.selectableNodes.Find(n => n.Id == nodeId);
-            var player = ConnectingAppManager.CharacterManager.GetPlayer();
-            player.AddMessage(charId, curDialog.currentNode);
-            if (curDialog.TakeNextNodes(nodeId) != null)
-            {
-                return curDialog.TakeNextNodes(nodeId);
-            }
-            else
-            {
-                activeMessageDialogs.Remove(dialogId);
-                return null;
-            }
-        }
+		public void BreakingDialog(string character, string dialogId, DialogueMode dialogueMode, EDialogueResultType breakingType)
+		{
+			var npc = ConnectingAppManager.CharacterManager.GetNPC(character);
+			npc.AvailableDialogs[dialogueMode].Find(s => s.Id.Equals(dialogId)).ActivateResult(breakingType);
+		}
 
-        public List<DialogueNode> ContinueDisscussion(string dialogId)
-        {
-            return discussions.Find(d => d.Id == dialogId).TakeNextNodes();
-        }
+		public void BreakingDialog(EDialogueResultType breakingType)
+		{
+			activeDialogs.Last().ActivateResult(breakingType);
+		}
 
-        public bool IsDialogLonely(Dialog dialog)
-        {
-            return activeDialogs.ToList().FindAll(s => s.Participants.First().Equals(dialog.Participants.First())).Count > 0;
-        }
-    }
+		public List<DialogueNode> ContinueMessengerDialog(int nodeId, string dialogId, string charId, DialogueMode mode)
+		{
+			Dialog curDialog = activeMessageDialogs[dialogId];
+			curDialog.currentNode = curDialog.selectableNodes.Find(n => n.Id == nodeId);
+			var player = ConnectingAppManager.CharacterManager.GetPlayer();
+			player.AddMessage(charId, curDialog.currentNode, mode);
+			if (curDialog.TakeNextNodes(nodeId) != null)
+			{
+				return curDialog.TakeNextNodes(nodeId);
+			}
+			else
+			{
+				activeMessageDialogs.Remove(dialogId);
+				return null;
+			}
+		}
+
+		public List<DialogueNode> ContinueDisscussion(string dialogId)
+		{
+			return discussions.Find(d => d.Id == dialogId).TakeNextNodes();
+		}
+
+		public bool IsDialogLonely(Dialog dialog)
+		{
+			return activeDialogs.ToList().FindAll(s => s.Participants.First().Equals(dialog.Participants.First())).Count > 0;
+		}
+
+		public List<DialogueNode> ContinueDialog(DialogueMode mode, int nodeId = -1, string dialogId = "", string charId = "", DialogueNode dialogueNode = null)
+		{
+			switch (mode)
+			{
+				case DialogueMode.call:
+					return ContinueDialog(dialogueNode);
+				case DialogueMode.email:
+				case DialogueMode.sms:
+					if (dialogId != "" && charId != "")
+					{
+						return ContinueMessengerDialog(nodeId, dialogId, charId, mode);
+					}
+					else { return null; }
+				case DialogueMode.videocall:
+					return ContinueDialog(dialogueNode);
+				case DialogueMode.meet:
+					return ContinueDialog(dialogueNode);
+				case DialogueMode.dialogueInterface:
+					return ContinueDialog(dialogueNode);
+				default:
+					return ContinueDialog(dialogueNode);
+			}
+		}
+
+	}
 }
