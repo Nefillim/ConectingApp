42c83b3a1f06b067754f20254b53990d532079a7
Виталий Онуфриенко <bumacka@gmail.com>
2018-08-24 17:43:17 +0300

    add date

--- ConnectingApplication/Managers/ConnectingAppManager.cs
+++ ConnectingApplication/Managers/ConnectingAppManager.cs
@@ -14,6 +14,7 @@ namespace ConnectingApplication.Managers
         public static EventResultsManager EventResultsManager { get; private set; }
         public static FlagManager FlagManager { get; private set; }
         public static SaveManager SaveManager { get; private set; }
+        public static int Date { get { return CoreController.TimeModule.GetDate(); } }
 
 
         private static void ExceptionListener(string message)
