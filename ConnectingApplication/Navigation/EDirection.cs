f107770935c916ad50c821d5682993320e403835
Виталий Онуфриенко <bumacka@gmail.com>
2018-09-05 16:45:49 +0300

    add fields for point

---
+++ ConnectingApplication/Navigation/EDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.ConectingApp.ConnectingApplication.Navigation
+{
+    public enum EDirection
+    {
+        north, northEast, east, southEast,
+        south, southWest, west, northWest
+    }
+}
--- ConnectingApplication/Navigation/Point.cs
+++ ConnectingApplication/Navigation/Point.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Assets.ConectingApp.ConnectingApplication.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.ConectingApp.ConnectingApplication.PathFinder
 {
     class Point
     {
+        public Vector3 Coordinates;
+        public Dictionary<EDirection, Point> Neighbors;
+        public float Weight;
     }
 }
