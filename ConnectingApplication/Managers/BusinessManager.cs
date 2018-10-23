7c2503bbca908fbd19a5734c0e3848d68d840be6
kaivyn.molis <kaivyn.molis@yandex.ru>
2018-10-23 12:56:27 +0300

    busines hotfix

--- ConnectingApplication/Managers/BusinessManager.cs
+++ ConnectingApplication/Managers/BusinessManager.cs
@@ -64,7 +64,8 @@ namespace ConnectingApplication.Managers
         public IList<Business> GetBusiness(bool forCalendar)
         {
             var businesses = availableBusiness;
-            if (forCalendar)
+			RemoveOldAndImpossibleBusinesses();
+			if (forCalendar)
             {
                 businesses = new List<string>();
                 businesses.AddRange(availableBusiness);
