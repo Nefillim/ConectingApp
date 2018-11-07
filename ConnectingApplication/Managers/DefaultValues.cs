950843fb073dc79e1328a8d44241b2c8100a24d1
Виталий Онуфриенко <bumacka@gmail.com>
2018-11-07 16:30:52 +0300

    add new default value

--- ConnectingApplication/Managers/DefaultValues.cs
+++ ConnectingApplication/Managers/DefaultValues.cs
@@ -13,6 +13,7 @@ namespace Assets.ConectingApp.ConnectingApplication.Managers
         public static readonly KeyCode PHONE_ENABLE_KEY = KeyCode.Q;
         public static readonly KeyCode TABLET_ENABLE_KEY = KeyCode.E;
         public static readonly KeyCode INTERACTION_KEY = KeyCode.F;
+        public static readonly KeyCode OPEN_TASKS_KEY = KeyCode.Tab;
         public static readonly int WAITING_SECONDS_BEFORE_CALL = 10;
         public static readonly int SEARCHING_DISTANCE = 10;
         public static readonly int MIN_DISTANCE_FOR_INTERACTION = 5;
