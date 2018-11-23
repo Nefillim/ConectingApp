11f6dad3779fd7cbaa9b58a75b41c56c7eb78958
Виталий Онуфриенко <bumacka@gmail.com>
2018-11-23 04:21:42 +0300

    change min distance for interaction

--- ConnectingApplication/Managers/PathManagerImpls/LocalizationType.cs
+++ ConnectingApplication/Managers/PathManagerImpls/LocalizationType.cs
@@ -1,8 +1,14 @@
-﻿namespace Assets.ConectingApp.ConnectingApplication.Managers.PathManagerImpls
+﻿using Assets.Scripts.Managers;
+using Assets.Scripts.Parsers.Interfaces;
+using System.Collections.Generic;
+
+namespace Assets.ConectingApp.ConnectingApplication.Managers.PathManagerImpls
 {
     public enum LocalizationType
     {
         Arts, Businesses, Characters, FriendFace, GameOver, Interactions,
         MiniGames, Notifications, PC, Phone, Replics, Screens, Tablet, Tasks
     }
+
+    
 }
